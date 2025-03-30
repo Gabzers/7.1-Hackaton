4d55c624a233a37dcfcecc3e2429f6cb72b0237e
andreferreira07 <47967@alunos.upt.pt>
2025-03-30 15:48:29 +0100

    aaa

---
+++ DataAnalysis/AmazonAnalysis.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os  # Import os for file existence check
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+# Load dataset
+def load_data(filepath):
+    if not os.path.exists(filepath):
+        raise FileNotFoundError(f"The file '{filepath}' does not exist. Please provide a valid path.")
+    data = pd.read_csv(filepath)
+
+# Load dataset
+filepath = 'DataAnalysis/AmazonSales.csv'
+data = load_data(filepath)
